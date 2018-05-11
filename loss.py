c429b43df8b24d2df06cd7297633e57105355737
Dibya Prakash Das <dibyadascool@gmail.com>
2018-05-12 04:57:13 +0530

    remove comments

--- loss.py
+++ loss.py
@@ -1,4 +1,4 @@
-# Originally written https://github.com/meetshah1995/pytorch-semseg
+# Author:- Dibya Prakash Das
 
 import torch
 import numpy as np
@@ -14,17 +14,3 @@ def cross_entropy2d(input, target):
     target = target.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
     return lossfunc(input, target)
 
-
-# def cross_entropy2d(input, target, weight=None, size_average=True):
-#     n, c, h, w = input.size()
-#     log_p = F.log_softmax(input)
-#     log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-#     log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
-#     log_p = log_p.view(-1, c)
-
-#     mask = target >= 0
-#     target = target[mask]
-#     loss = F.nll_loss(log_p, target, weight=weight, size_average=False)
-#     if size_average:
-#         loss /= mask.data.sum()
-#     return loss
